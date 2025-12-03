79f022b80e20dc82a6341ba791c745c458ab170a
nnilayy <nnilayy.work@gmail.com>
2025-12-04 02:29:06 +0530

    refactor: backend

--- prompts/__init__.py
+++
@@ -1,6 +0,0 @@
-# Prompts package for Samora AI
-
-from prompts.system_prompt import SYSTEM_PROMPT
-from prompts.user_idle_prompts import USER_IDLE_PROMPTS
-from prompts.wake_prompts import WAKE_PROMPTS
-from prompts.function_descriptions import HOLD_FUNCTION_DESCRIPTION, END_CALL_FUNCTION_DESCRIPTION
